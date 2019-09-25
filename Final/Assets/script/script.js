fed6845c34cba6d6f5a4ae913f265b904caa0d52
kenniscky <kwinki@163.com>
2019-09-25 12:47:09 -0400

    add jquery - about me

--- Final/Assets/script/script.js
+++ Final/Assets/script/script.js
@@ -1,1 +1,16 @@
 console.log("Final Project is Working");
+
+// from in class example. not working??
+var myIntro = document.getElementById("myIntro");
+
+function openMe(){
+	// mySideNav.style.width="30vw";
+	$("#myIntro").addClass("openMe");
+}
+
+function closeMe(){
+	// mySideNav.style.width="0";
+	$("#myIntro").removeClass("openMe");
+}
+
+
